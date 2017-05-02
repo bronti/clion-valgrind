ef3aeb01d19bcd8b6fa9e8dae74e29edfd318c87
bronti <anya.yaveyn@yandex.ru>
2017-05-02 15:28:25 +0300

    simple clion plugin

---
+++ src/ru/spbau/devdays/clionvalgrind/DoSomething.java
@@ -0,0 +1,17 @@
+package ru.spbau.devdays.clionvalgrind;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+
+/**
+ * Created by bronti on 02.05.17.
+ */
+
+public class DoSomething extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        throw new UnsupportedOperationException();
+    }
+}
